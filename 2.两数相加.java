e345aa471f0cbd25a42ebf67c0e0154ae25b285f
owlfeng <1033571362@qq.com>
2019-06-06 23:22:54 +0800

    第2题

--- 2.两数相加.java
+++ 2.两数相加.java
@@ -4,17 +4,38 @@
  * [2] 两数相加
  */
 /**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) { val = x; }
- * }
+ * Definition for singly-linked list. public class ListNode { int val; ListNode
+ * next; ListNode(int x) { val = x; } }
  */
 class Solution {
+    /**
+     * 
+     * @param l1
+     * @param l2
+     * @return
+     */
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-   // 明天写这个     
-        
-    }
-}
+        // 思路一
+        // 先声明两个ListNote
+        int carry = 0;
+        ListNode p, dummy = new ListNode(0);
+        p = dummy;
+        while (l1 != null || l2 != null || carry != 0) {
+            if (l1 != null) {
+                carry += l1.val;
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                carry += l2.val;
+                l2 = l2.next;
+            }
+            // 取个位数
+            p.next = new ListNode(carry % 10);
+            carry /= 10;
+            p = p.next;
 
+        }
+        return dummy.next;
+    }
+
+}
